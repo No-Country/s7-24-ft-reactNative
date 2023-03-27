e7939967060d32b1d57da3f405f0feff0b27a829
SeabastianDeLaCruz <sebastiandelacruz335@gmial.com>
2023-03-27 15:40:19 -0300

    init project

---
+++ App/src/firebase/app.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAhbNBPL2U3GWRKHjEp0E3JPyDcKtSmSW8",
+  authDomain: "nc-reactnative.firebaseapp.com",
+  projectId: "nc-reactnative",
+  storageBucket: "nc-reactnative.appspot.com",
+  messagingSenderId: "619182330674",
+  appId: "1:619182330674:web:3bdb2d20ec642ad17c7d2b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
