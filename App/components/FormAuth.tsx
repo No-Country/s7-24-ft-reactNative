5a79abd354dd2f6c21b9e2c547cba1980dd613ed
SeabastianDeLaCruz <sebastiandelacruz335@gmial.com>
2023-03-27 23:14:51 -0300

    init project

---
+++ App/components/FormAuth.tsx
@@ -0,0 +1,8 @@
+import { View } from "react-native";
+import Children from "../models/children.models";
+
+const FormAuth = ({ children }: Children) => {
+	return <View>{children}</View>;
+};
+
+export default FormAuth;
